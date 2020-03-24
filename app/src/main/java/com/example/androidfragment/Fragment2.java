a520a361bf60ab79d111be9e73f4f4bb1f1ae499
Vihanga1997 <vihangavithana@gmail.com>
2020-03-24 20:10:35 +0530

    Tutorial03

---
+++ app/src/main/java/com/example/androidfragment/Fragment2.java
@@ -0,0 +1,17 @@
+package com.example.androidfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
